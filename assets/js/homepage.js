5065a30c9538a8b4794fd2dfc4eea0a3cc2be63b
Vincent Toon <vincenttoon22@gmail.com>
2022-08-16 01:45:49 -0500

    this wasn't very clear

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -47,12 +47,14 @@ var displayRepos = function(repos, searchTerm) {
     repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
-    for (let i = 0; i < repos.length; i++) {
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
-
-        // create container for each repo
-        var repoEl = document.createElement("div");
+        
+        // create a container for each repo
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         // create a span element to hold repo name
         var titleEl = document.createElement("span");
